ad6c785e3ca21f8e3f322624e8419803b4900a26
Anna Tsyganova <anna.tsyganova.dev@gmail.com>
2026-06-03 16:22:56 +0300

    Разбила файл с переводами на не несколько файлов

--- src/i18n/language.ts
+++ src/i18n/language.ts
@@ -1,11 +1,23 @@
-import en from "./language/en.json";
-import ru from "./language/ru.json";
-
 import { Locale } from "./config";
+import en from "./language/en.json";
+import privacyEn from "./language/en.privacy.json";
+import projectsEn from "./language/en.projects.json";
+
+import ru from "./language/ru.json";
+import privacyRu from "./language/ru.privacy.json";
+import projectsRu from "./language/ru.projects.json";
 
 export const messagesByLocale: Record<Locale, typeof ru> = {
-  ru,
-  en,
+  ru: {
+    ...ru,
+    Projects: { ...ru.Projects, ...projectsRu },
+    Privacy: { ...ru.Privacy, ...privacyRu },
+  },
+  en: {
+    ...en,
+    Projects: { ...en.Projects, ...projectsEn },
+    Privacy: { ...en.Privacy, ...privacyEn },
+  },
 };
 
 export function getMessages(locale: Locale): typeof ru {
